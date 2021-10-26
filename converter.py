fc180ae428dbc35f194ab820f58bd5618da93ca9
Albert Hovakimyan <HovakimyanAB@gmail.com>
2021-10-27 02:53:02 +0400

    add w d conv wd

--- converter.py
+++ converter.py
@@ -1,9 +1,33 @@
 import main
 
 
+class wdconv:
+    units = {
+        'kilogram': ('weight', 1.),
+        'pound': ('weight', 0.453592),
+        'meter': ('distance', 1.),
+        'kilometer': ('distance', 1000.),
+        'centimeter': ('distance', 0.01),
+        'millimeter': ('distance', 0.001),
+        'mile': ('distance', 1609.35),
+        'feet': ('distance', 0.3048),
+        'inch': ('distance', 0.0254),
+        'B': ('data', 1),
+        'KB': ('data', 1024),
+        'MB': ('data', 1024.0**2),
+        'GB': ('data', 1024.0**3),
+        'TB': ('data', 1024.0**4),
+        'PB': ('data', 1024.0**5)
+
+    }
+
+    def convert(param, in_param, out_param):
+        return (param*(wdconv.units[in_param])[1])/(wdconv.units[out_param][1])
+
+
 def ConvMenu(self):
     main.Log_writer.write(self, "open converter menu")
-    print("What you are wont to convert?\na. Data,\nb. Body Mass Index,\nc. Age,\nd. Lenght,\ne. Area,\nf. Discount\n")
+    print("-----What you are wont to convert?----\na. Data,\nb. Body Mass Index,\nc. Age,\nd. Lenght,\ne. Area,\nf. Discount\n")
     m = input("enter char of your choose\n")
     if m == "a":
         Dataconv(self)
@@ -20,8 +44,9 @@ def Dataconv(self):
     print("Choose type of output valve")
     outdatatype = input("(Enter B,KB,MB,GB,TB or PB)")
     main.Log_writer.write(self, ("wont convert "+str(valve)+str(indatatype)+" to "+str(outdatatype)+"`s"))
-    print(formatFileSize(valve, indatatype, outdatatype))
-    main.Log_writer.write(self, ("convert result = "+str(formatFileSize(valve, indatatype, outdatatype))))
+    print(valve, indatatype, " = ", wdconv.convert(valve, indatatype, outdatatype), outdatatype)
+    main.Log_writer.write(self, ("convert result = "+str(wdconv.convert(valve, indatatype, outdatatype))))
+    back = input("enter for go to converter menu")
     ConvMenu(self)
 
 
@@ -35,80 +60,6 @@ def convertFloatToDecimal(f=0.0, precision=2):
     return ("%." + str(precision) + "f") % f
 
 
-def formatFileSize(size, sizeIn, sizeOut):
-    '''
-    Convert file size to a string representing its value in B, KB, MB and GB.
-    The convention is based on sizeIn as original unit and sizeOut
-    as final unit.
-    '''
-    assert sizeIn.upper() in {"B", "KB", "MB", "GB", "TB", "PB"}, "sizeIn type error"
-    assert sizeOut.upper() in {"B", "KB", "MB", "GB", "TB", "PB"}, "sizeOut type error"
-    if sizeIn == "B":
-        if sizeOut == "KB":
-            return convertFloatToDecimal((size/1024.0))
-        elif sizeOut == "MB":
-            return convertFloatToDecimal((size/1024.0**2))
-        elif sizeOut == "GB":
-            return convertFloatToDecimal((size/1024.0**3))
-        elif sizeOut == "TB":
-            return convertFloatToDecimal((size/1024.0**4))
-        elif sizeOut == "PB":
-            return convertFloatToDecimal((size/1024.0**5))
-    elif sizeIn == "KB":
-        if sizeOut == "B":
-            return convertFloatToDecimal((size*1024.0))
-        elif sizeOut == "MB":
-            return convertFloatToDecimal((size/1024.0))
-        elif sizeOut == "GB":
-            return convertFloatToDecimal((size/1024.0**2))
-        elif sizeOut == "TB":
-            return convertFloatToDecimal((size/1024.0**3))
-        elif sizeOut == "PB":
-            return convertFloatToDecimal((size/1024.0**4))
-    elif sizeIn == "MB":
-        if sizeOut == "B":
-            return convertFloatToDecimal((size*1024.0**2))
-        elif sizeOut == "KB":
-            return convertFloatToDecimal((size*1024.0))
-        elif sizeOut == "GB":
-            return convertFloatToDecimal((size/1024.0))
-        elif sizeOut == "TB":
-            return convertFloatToDecimal((size/1024.0**2))
-        elif sizeOut == "PB":
-            return convertFloatToDecimal((size/1024.0**3))
-    elif sizeIn == "GB":
-        if sizeOut == "B":
-            return convertFloatToDecimal((size*1024.0**3))
-        elif sizeOut == "KB":
-            return convertFloatToDecimal((size*1024.0**2))
-        elif sizeOut == "MB":
-            return convertFloatToDecimal((size*1024.0))
-        elif sizeOut == "TB":
-            return convertFloatToDecimal((size/1024.0))
-        elif sizeOut == "PB":
-            return convertFloatToDecimal((size/1024.0**2))
-    elif sizeIn == "TB":
-        if sizeOut == "B":
-            return convertFloatToDecimal((size*1024.0**4))
-        elif sizeOut == "KB":
-            return convertFloatToDecimal((size*1024.0**3))
-        elif sizeOut == "MB":
-            return convertFloatToDecimal((size*1024.0**2))
-        elif sizeOut == "GB":
-            return convertFloatToDecimal((size*1024.0))
-        elif sizeOut == "PB":
-            return convertFloatToDecimal((size/1024.0))
-    elif sizeIn == "PB":
-        if sizeOut == "B":
-            return convertFloatToDecimal((size*1024.0**5))
-        elif sizeOut == "KB":
-            return convertFloatToDecimal((size*1024.0**4))
-        elif sizeOut == "MB":
-            return convertFloatToDecimal((size*1024.0**3))
-        elif sizeOut == "GB":
-            return convertFloatToDecimal((size*1024.0**2))
-        elif sizeOut == "TB":
-            return convertFloatToDecimal((size*1024.0))
 
 
 def bmi(self):
@@ -122,21 +73,3 @@ def bmi(self):
         wdconv.convert(amt, 'kg', 'pound')
     if m == "2":
         pass
-
-class wdconv:
-    units = {
-        'kilogram': ('weight', 1.),
-        'pound': ('weight', 0.453592),
-        'meter': ('distance', 1.),
-        'kilometer': ('distance', 1000.),
-        'centimeter': ('distance', 0.01),
-        'millimeter':('distance', 0.001),
-        'mile': ('distance', 1609.35),
-        'feet': ('distance', 0.3048),
-        'inch': ('distance', 0.0254)
-    }
-
-    def convert(param, in_param, out_param):
-        return (param*(wdconv.units[in_param])[1])/(wdconv.units[out_param][1])
-
-print(wdconv.convert(5, 'feet', 'mile'))
